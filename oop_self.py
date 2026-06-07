835d09feb750aa837da54abf9452108bb05bd031
Amresh04 <kumaramresh0174@gmail.com>
2026-06-07 17:45:56 +0530

    Add employee class with salary and language attributes

---
+++ oop_self.py
@@ -0,0 +1,7 @@
+class employee:
+    language="python"
+    salary=1200000
+    def getInfo(self):
+        print(f"the language is{self.language}.the salary is {self.salary}")
+amresh=employee()
+amresh.getInfo() 
